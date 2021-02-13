b2014216ec62c3666e095e237b6135392a9354d4
Tony Duong <tony.duong.102@gmail.com>
2021-02-13 20:14:02 +0900

    Added: do not show stripe button when no items in cart

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -40,7 +40,12 @@ const CheckoutPage = ({ cartItems, total }) => (
       <br />
       4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
     </div>
-    <StripeCheckoutButton price={total} />
+    {
+      cartItems.length > 0 ?
+        <StripeCheckoutButton price={total} />
+      :
+        ''
+    }
   </div>
 );
 
